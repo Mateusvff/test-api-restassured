786109e0acb3ece4107f6e007cf34aed0a79c17d
Mateus Franco <mateus.franco@singletech.com.br>
2022-03-02 13:55:30 -0300

    OlaMundo response

--- src/main/java/secaoBasica/OlaMundo.java
+++ src/main/java/secaoBasica/OlaMundo.java
@@ -1,2 +1,14 @@
-package secaoBasica;public class OlaMundo {
+package secaoBasica;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class OlaMundo {
+
+        public static void main(String[] args){
+            Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+            System.out.println(response.getBody().asString());
+        }
+
 }
